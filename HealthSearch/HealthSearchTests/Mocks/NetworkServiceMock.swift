3d492faaf1dee70c3e496120da1bdb2a824d53e8
vitorpiresc <vitorpiresc@gmail.com>
2024-06-05 09:46:36 -0300

    chore: adds view tests

--- HealthSearch/HealthSearchTests/Mocks/NetworkServiceMock.swift
+++ HealthSearch/HealthSearchTests/Mocks/NetworkServiceMock.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+@testable import HealthSearch
+
+final class NetworkServiceMock: NetworkService {
+    var loadWasCalled = 0
+    func load() async throws -> ProviderModel {
+        loadWasCalled += 1
+        return ProviderModel(data: [])
+    }
+}
